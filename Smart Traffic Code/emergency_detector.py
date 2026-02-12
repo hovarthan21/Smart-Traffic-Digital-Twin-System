91dfd2519ff3d285159ecfbbe00feacff0a0f1f6
HOVARTHAN S <hovarthan04@gmail.com>
2026-02-12 13:48:08 +0530

    Delete Smart Traffic Code directory

--- Smart Traffic Code/emergency_detector.py
+++
@@ -1,6 +0,0 @@
-def detect_emergency(vehicle_count):
-   
-    if vehicle_count > 80:
-        return "⚠ Possible Emergency Congestion"
-    return "No Emergency"
-
--- Smart Traffic Code/signal_optimizer.py
+++
@@ -1,7 +0,0 @@
-def optimize_signal(vehicle_count):
-    if vehicle_count > 50:
-        return "🚦 Extend Green Signal (60 sec)"
-    elif vehicle_count > 20:
-        return "🚦 Normal Green Signal (40 sec)"
-    else:
-        return "🚦 Short Green Signal (20 sec)"
